0f0e19ed47243ecba55cd6fc364f61826ff49928
Molly McCarron <mccarronmollye@gmail.com>
2018-11-28 16:31:40 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,52 @@
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by {|element| element.length}
+end
+
+def swap_elements(array)
+  array[1], array [2] =   array[2],  array [1]
+  array
+  #array [1] = array [2]
+  #array [2] = array [1] Is pretty meta. The second statement essentially undoes our first staement.
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.select do |name|
+    name.split(",")
+    name[2] = "$"
+  end
+  array
+end
+
+def find_a(array)
+  array.select do |element|
+    element.split(",")
+    element[0].eql? "a"
+  end
+end
+
+def sum_array(array)
+  array.reduce (:+)
+end
+
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
+end
